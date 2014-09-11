6ad2603e8816d847846f3187d90df214b34c44dd
Jolo Balbin <jolo@jpbalb.in>
2014-09-11 11:12:12 +0800

    changed function name for Cosine Similarity

--- Python/cosine_similarity.py
+++ Python/cosine_similarity.py
@@ -1,10 +1,13 @@
+import re, math
+from collections import Counter
+
 class CosineSimilarity:
-  def get_cosine(vec1, vec2):
+  def getCosine(self, vec1, vec2):
     intersection = set(vec1.keys()) & set(vec2.keys())
     numerator = sum([vec1[x] * vec2[x] for x in intersection])
 
-    sum1 = sum([vec1[x]**2 for x in vec1.keys()])
-    sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+    sum1 = sum([vec1[x] ** 2 for x in vec1.keys()])
+    sum2 = sum([vec2[x] ** 2 for x in vec2.keys()])
     denominator = math.sqrt(sum1) * math.sqrt(sum2)
 
     if not denominator:
@@ -12,7 +15,7 @@ class CosineSimilarity:
     else:
       return float(numerator) / denominator
 
-  def text_to_vector(text):
+  def textToVector(self, text):
     wordRegex = re.compile(r'\w+')
 
     words = wordRegex.findall(text)
